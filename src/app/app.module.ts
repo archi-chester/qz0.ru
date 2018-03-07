b58bacea317668378c35b6dc5097741c0f0d212a
chester <me@qz0.ru>
2018-03-07 23:13:57 +0300

    11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import {NotFoundPage} from './pages/not_found_page/not.found.page';
+import {StartPage} from './pages/start_page/start.page';
+import {LoginPage} from './pages/login_page/login.page';
+import {RouterModule} from '@angular/router';
+import {appRoutes} from './app.routing';
+import {AppIndex} from './app_index/app.index';
+import {ServerDataService} from './work_with_server/server.data.service';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppIndex,
+    LoginPage,
+    NotFoundPage,
+    StartPage,
   ],
   imports: [
-    BrowserModule
+    AlertModule.forRoot(),
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {useHash: true}),
+    AlertModule.forRoot(),
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+        ServerDataService,
+  ],
+  bootstrap: [AppIndex],
 })
 export class AppModule { }
