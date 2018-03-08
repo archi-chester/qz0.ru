3c73113b47cbbed1bde27e100b975ad1f2e9566a
Archi Chester <me@qz0.ru>
2018-03-08 14:32:47 +0300

    Сделал каркас
    Signed-off-by: Archi Chester <me@qz0.ru>

--- src/app/elements/menu-cmp/menu-cmp.component.ts
+++ src/app/elements/menu-cmp/menu-cmp.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Paths} from '../../paths/paths';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-menu-cmp',
@@ -7,9 +9,28 @@ import {Component, OnInit} from '@angular/core';
 })
 export class MenuCmpComponent implements OnInit {
 
-  constructor() { }
+  //  Приватные переменные
+  private router: Router;
+
+  constructor(router: Router) {
+    this.router = router;
+  }
 
   ngOnInit() {
   }
 
+  // переход на стартовую страницу
+  public goToStartPage(): void {
+      this.router.navigate(['/' + new Paths().startPage.path]);
+  }
+
+  // переход на страницу новостей
+  public goToNewsPage(): void {
+      this.router.navigate(['/' + new Paths().newsPage.path]);
+  }
+
+  // переход на страницу постов
+  public goToPostsPage(): void {
+      this.router.navigate(['/' + new Paths().postsPage.path]);
+  }
 }
