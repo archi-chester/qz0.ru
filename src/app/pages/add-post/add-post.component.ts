7aeb07d79149aedc2bb909e8290a1686ca7ddf7d
Archi Chester <me@qz0.ru>
2018-03-09 23:56:52 +0300

    Написаны заглушки функций
    Signed-off-by: Archi Chester <me@qz0.ru>

--- src/app/pages/add-post/add-post.component.ts
+++ src/app/pages/add-post/add-post.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ServerDataService} from '../../work_with_server/server.data.service';
 import {IPosts} from '../../app.types';
 
@@ -8,15 +8,35 @@ import {IPosts} from '../../app.types';
   styleUrls: ['./add-post.component.css']
 })
 export class AddPostComponent implements OnInit {
-  //  Защищенные переменные
-  public posts: IPosts[];
+  //  Общие переменные
+  @Input() public post: IPosts = {
+    _id: '', //  Id
+    posttitle: '', // заголовок
+    createdate: '', // дата создания
+    posttags: [], // тэги
+    postbody: '', // тело
+    src_picture: '', //  картинка
+  };
   //  Приватные переменные
   private service: ServerDataService; // сервис для работы с бэкэндом
 
+  //  конструктор
   constructor(service: ServerDataService) {
         this.service = service;
   }
+
+  //  инициализация
   ngOnInit() {
+  }
+
+  //  отправка поста сервису
+  public addPost(): void {
+    //
+  }
+
+  //  добавить тег к массиву
+  public addTag(tag: string): void {
+    //
   }
 
 }
